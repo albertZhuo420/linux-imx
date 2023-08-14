411f981154c9cd8904d77924876d651e4132d9f2
albertZhuo420 <mr.zhuo420@outlook.com>
2023-08-14 16:01:19 +0800

    通过命令行传参来指定busybox还是systemd

--- my_install_modules.sh
+++ my_install_modules.sh
@@ -1,3 +1,29 @@
 #!/bin/env bash
 
-make modules_install INSTALL_MOD_PATH=/home/zhuo/work/server-dir/nfs/rootfs/buildroot-2023.05.1/busybox/usr
+function print_usage()
+{
+	echo "***************Usage Start ***************"
+	echo "	./shell.sh b: Use busybox"
+	echo "		or"
+	echo "	./shell.sh s: Use systemd"
+	echo "***************Usage End *****************"
+}
+
+BUSYBOX_FS_PATH="/home/zhuo/work/server-dir/nfs/rootfs/buildroot-2023.05.1/busybox/usr"
+SYSTEMD_FS_PATH="/home/zhuo/work/server-dir/nfs/rootfs/buildroot-2023.05.1/systemd/usr"
+
+if [ $# == 0 ]; then
+	print_usage;
+	exit 1
+else
+	if [ $1 == 'b' ]; then 
+		make modules_install INSTALL_MOD_PATH=$BUSYBOX_FS_PATH
+		# echo $BUSYBOX_FS_PATH
+	elif [ $1 == 's' ]; then
+		make modules_install INSTALL_MOD_PATH=$SYSTEMD_FS_PATH
+		# echo $SYSTEMD_FS_PATH
+	else
+		print_usage
+		exit 2
+	fi
+fi
