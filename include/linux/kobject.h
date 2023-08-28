3f2b35439d43815e3ea3e020aea983591ecca8d9
albertZhuo420 <mr.zhuo420@outlook.com>
2023-08-28 17:28:25 +0800

    struct kobject: 注释 uevent_suppress 字段

--- include/linux/kobject.h
+++ include/linux/kobject.h
@@ -76,7 +76,7 @@ struct kobject {
 	unsigned int state_in_sysfs:1;
 	unsigned int state_add_uevent_sent:1;
 	unsigned int state_remove_uevent_sent:1;
-	unsigned int uevent_suppress:1;
+	unsigned int uevent_suppress:1; // 如果这一位被设置为1, 那么该kobj的uevent就不会被执行
 };
 
 extern __printf(2, 3)
