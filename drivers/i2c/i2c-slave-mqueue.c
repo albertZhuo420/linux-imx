233812eb8caaac7f78a8ab2bd5ba5d7b88d6e399
albertZhuo420 <mr.zhuo420@outlook.com>
2023-09-04 10:24:03 +0800

    代码注释以及格式化

--- drivers/i2c/i2c-slave-mqueue.c
+++ drivers/i2c/i2c-slave-mqueue.c
@@ -13,9 +13,11 @@
  * 在执行完 make defconfig 命令后, 会在/linux-6.0.9/include/generated
  * 目录下生成一个 [autoconfig.h], 改头文件中会保存与[.config]文件对应的
  * CONFIG信息, 比如下面的 CONFIG_I2C_SLAVE_MQUEUE_MESSAGE_SIZE;
+ * 
+ * MQ_QUEUE_SIZE 改成 MQ_QUEUE_CNT 更好;
 */
-#define MQ_MSGBUF_SIZE		CONFIG_I2C_SLAVE_MQUEUE_MESSAGE_SIZE // 256
-#define MQ_QUEUE_SIZE		CONFIG_I2C_SLAVE_MQUEUE_QUEUE_SIZE	 // 32
+#define MQ_MSGBUF_SIZE		CONFIG_I2C_SLAVE_MQUEUE_MESSAGE_SIZE // default 120
+#define MQ_QUEUE_SIZE		CONFIG_I2C_SLAVE_MQUEUE_QUEUE_SIZE	 // default 32
 #define MQ_QUEUE_NEXT(x)	(((x) + 1) & (MQ_QUEUE_SIZE - 1))
 
 struct mq_msg {
@@ -24,19 +26,6 @@ struct mq_msg {
 };
 
 struct mq_queue {
-	/**
-	 * // include\linux\sysfs.h
-	 * 
-	 * struct bin_attribute {
-	 * 		struct attribute	attr;
-	 * 		size_t			size;
-	 * 		void			*private;
-	 * 		struct address_space *(*f_mapping)(void);
-	 * 		ssize_t (*read)(struct file *, struct kobject *, struct bin_attribute *, char *, loff_t, size_t);
-	 * 		ssize_t (*write)(struct file *, struct kobject *, struct bin_attribute *, char *, loff_t, size_t);
-	 * 		int (*mmap)(struct file *, struct kobject *, struct bin_attribute *attr, struct vm_area_struct *vma);
-	 * };
-	*/
 	struct bin_attribute bin;
 	struct kernfs_node *kn;
 	struct i2c_client *client; // i2c slave device;
@@ -73,51 +62,55 @@ static int i2c_slave_mqueue_callback(struct i2c_client *client,
 	int ret = 0;
 
 	switch (event) {
-	case I2C_SLAVE_WRITE_REQUESTED:
-	case I2C_SLAVE_GCALL_REQUESTED:
-		mq->truncated = 0;
+		case I2C_SLAVE_WRITE_REQUESTED:
+		case I2C_SLAVE_GCALL_REQUESTED:
+			mq->truncated = 0;
 
-		msg->len = 1;
-		msg->buf[0] = event == I2C_SLAVE_GCALL_REQUESTED ?
-			      0 : client->addr << 1;
-		break;
-
-	case I2C_SLAVE_WRITE_RECEIVED:
-		if (msg->len < MQ_MSGBUF_SIZE) {
-			msg->buf[msg->len++] = *val;
-		}
-		else {
-			dev_err(&client->dev, "message is truncated!\n");
-			mq->truncated = 1;
-			ret = -EINVAL;
-		}
-		break;
-
-	case I2C_SLAVE_STOP:
-		if (unlikely(mq->truncated || msg->len < 2))
+			msg->len = 1;
+			msg->buf[0] = (event == I2C_SLAVE_GCALL_REQUESTED) ?
+					      0 :
+					      client->addr << 1;
 			break;
 
-		spin_lock(&mq->lock);
-		mq->in = MQ_QUEUE_NEXT(mq->in);
-		mq->curr = &mq->queue[mq->in];
-		mq->curr->len = 0;
+		case I2C_SLAVE_WRITE_RECEIVED:
+			if (msg->len < MQ_MSGBUF_SIZE) {
+				msg->buf[msg->len++] = *val;
+			}
+			else {
+				dev_err(&client->dev, "message is truncated!\n");
+				mq->truncated = 1;
+				ret = -EINVAL;
+			}
+			break;
 
-		/* Flush the oldest message */
-		if (mq->out == mq->in)
-			mq->out = MQ_QUEUE_NEXT(mq->out);
-		spin_unlock(&mq->lock);
+		case I2C_SLAVE_STOP:
+			if (unlikely(mq->truncated || msg->len < 2))
+				break;
 
-		kernfs_notify(mq->kn);
-		break;
+			spin_lock(&mq->lock);
+			mq->in = MQ_QUEUE_NEXT(mq->in);
+			mq->curr = &mq->queue[mq->in];
+			mq->curr->len = 0;
 
-	default:
-		*val = 0xFF;
-		break;
+			/* Flush the oldest message */
+			if (mq->out == mq->in)
+				mq->out = MQ_QUEUE_NEXT(mq->out);
+			spin_unlock(&mq->lock);
+
+			kernfs_notify(mq->kn);
+			break;
+
+		default:
+			*val = 0xFF;
+			break;
 	}
 
 	return ret;
 }
 
+/**
+ * 该函数是给应用层来使用的;
+*/
 static ssize_t i2c_slave_mqueue_bin_read(struct file *filp,
 					 struct kobject *kobj,
 					 struct bin_attribute *attr,
@@ -139,7 +132,8 @@ static ssize_t i2c_slave_mqueue_bin_read(struct file *filp,
 			ret = msg->len;
 			memcpy(buf, msg->buf, ret);
 			I2C_HEX_DUMP(mq->client, buf, ret);
-		} else {
+		} 
+		else {
 			ret = -EOVERFLOW; /* Drop this HUGE one. */
 		}
 
@@ -149,8 +143,9 @@ static ssize_t i2c_slave_mqueue_bin_read(struct file *filp,
 	}
 	spin_unlock_irqrestore(&mq->lock, flags);
 
-	if (more)
+	if (more) {
 		kernfs_notify(mq->kn);
+	}
 
 	return ret;
 }
@@ -158,60 +153,73 @@ static ssize_t i2c_slave_mqueue_bin_read(struct file *filp,
 static int i2c_slave_mqueue_probe(struct i2c_client *client,
 				  const struct i2c_device_id *id)
 {
-	struct device *dev = &client->dev; // 获取 struct i2c_client中的dev字段指针;
+	// 获取 struct i2c_client中的struct device dev字段指针;
+	struct device *dev = &client->dev;
 	struct mq_queue *mq;
 	int ret, i;
 	void *buf;
 
 	mq = devm_kzalloc(dev, sizeof(*mq), GFP_KERNEL);
-	if (!mq)
+	if (!mq) {
 		return -ENOMEM;
+	}
 
 	BUILD_BUG_ON(!is_power_of_2(MQ_QUEUE_SIZE));
 
-	mq->client = client; // 就是设备节点: i2c-slave-mqueue@13(设备树中的信息)
+	// 就是设备节点: i2c-slave-mqueue@13(设备树中的信息)
+	mq->client = client; 
 
 	/**
 	 * 申请 MQ_QUEUE_SIZE 个 MQ_MSGBUF_SIZE 大小的内存空间;
 	*/
 	buf = devm_kmalloc_array(dev, MQ_QUEUE_SIZE, MQ_MSGBUF_SIZE,
-				 GFP_KERNEL);
-	if (!buf)
+				 			 GFP_KERNEL);
+	if (!buf) {
 		return -ENOMEM;
+	}
 
 	mq->queue = devm_kzalloc(dev, sizeof(*mq->queue) * MQ_QUEUE_SIZE,
-				 GFP_KERNEL);
-	if (!buf)
+				 			 GFP_KERNEL);
+	if (!buf) {
 		return -ENOMEM;
+	}
 
-	for (i = 0; i < MQ_QUEUE_SIZE; i++)
+	for (i = 0; i < MQ_QUEUE_SIZE; i++) {
 		mq->queue[i].buf = buf + i * MQ_MSGBUF_SIZE;
+	}
 
 	/**
 	 * struct i2c_client -> struct device -> void *driver_data;
 	 * client->dev->driver_data
 	 * Driver data, set and get with dev_set_drvdata/dev_get_drvdata
 	*/
-	i2c_set_clientdata(client, mq);
+	i2c_set_clientdata(client, mq); // 这样就将 devm_kzalloc()申请的mq与client绑定了;
 
 	spin_lock_init(&mq->lock);
 	mq->curr = &mq->queue[0];
 
-	sysfs_bin_attr_init(&mq->bin);
+	sysfs_bin_attr_init(&mq->bin); // 可能只是执行了 do {} while (0);
 	mq->bin.attr.name = "slave-mqueue";
 	mq->bin.attr.mode = 0400;
 	mq->bin.read = i2c_slave_mqueue_bin_read;
 	mq->bin.size = MQ_MSGBUF_SIZE * MQ_QUEUE_SIZE;
 
+	/**
+	 * 设备树静态指定, 下面的打印信息: 
+	 *   dev->kobj name: 1-1013
+	*/
+	printk("dev->kobj name: %s", dev->kobj.name);
 	ret = sysfs_create_bin_file(&dev->kobj, &mq->bin);
-	if (ret)
+	if (ret) {
 		return ret;
+	}
 
 	mq->kn = kernfs_find_and_get(dev->kobj.sd, mq->bin.attr.name);
 	if (!mq->kn) {
 		sysfs_remove_bin_file(&dev->kobj, &mq->bin);
 		return -EFAULT;
 	}
+	printk("VISCORE kn->name: %s", mq->kn->name);
 
 	ret = i2c_slave_register(client, i2c_slave_mqueue_callback);
 	if (ret) {
@@ -236,27 +244,10 @@ static void i2c_slave_mqueue_remove(struct i2c_client *client)
 }
 
 /**
- * 设备树示例:
- * 
- * // i2c#, # is count from 0
- * 
- * &i2c1 {
- *    status = "okay";
- *    // To PCIe slot SMBUS
- *    
- *    // 静态指定
- *    i2c-slave-mqueue@13 {
- *    	compatible = "slave-mqueue";
- *    	reg = <(0x13 | I2C_OWN_SLAVE_ADDRESS)>;
- *    	i2c-protocol;
- *    };
- * 
- * 	  // 动态指定:
- * 		参考链接: https://www.kernel.org/doc/html/latest/i2c/i2c-sysfs.html
- * 		# 将bmcSlaveAddr为0x13的设备挂载到i2c-1下面;
- *		$ echo "slave-mqueue 0x1013" > /sys/bus/i2c/devices/i2c-1/new_device
+ * struct i2c_device_id {
+ * 		char name[I2C_NAME_SIZE];
+ * 		kernel_ulong_t driver_data;	// Data private to the driver
  * };
- * 
 */
 static const struct i2c_device_id i2c_slave_mqueue_id[] = {
 	{ "slave-mqueue", 0 },
@@ -265,6 +256,16 @@ static const struct i2c_device_id i2c_slave_mqueue_id[] = {
 MODULE_DEVICE_TABLE(i2c, i2c_slave_mqueue_id);
 
 #if IS_ENABLED(CONFIG_OF)
+/*
+ * // Struct used for matching a device
+ *
+ *	struct of_device_id {
+ *		char	name[32];
+ *		char	type[32];
+ *		char	compatible[128];
+ *		const void *data;
+ *	};
+ */
 static const struct of_device_id i2c_slave_mqueue_of_match[] = {
 	{ .compatible = "slave-mqueue", .data = (void *)0 },
 	{ },
@@ -274,7 +275,7 @@ MODULE_DEVICE_TABLE(of, i2c_slave_mqueue_of_match);
 
 static struct i2c_driver i2c_slave_mqueue_driver = {
 	/**
-	 * struct i2c_driver 中的 struct device_driver driver;
+	 * struct i2c_driver 中的 struct device_driver driver字段;
 	*/
 	.driver = {
 		.name	= "i2c-slave-mqueue",
@@ -284,10 +285,23 @@ static struct i2c_driver i2c_slave_mqueue_driver = {
 	.remove		= i2c_slave_mqueue_remove,
 	.id_table	= i2c_slave_mqueue_id,
 };
+
 /**
  * #define module_i2c_driver(__i2c_driver) \
-				module_driver(__i2c_driver, i2c_add_driver, \
-							  i2c_del_driver)
+ *				module_driver(__i2c_driver, i2c_add_driver, \
+ *							  i2c_del_driver)
+ *
+ *	#define module_driver(__driver, __register, __unregister, ...) \
+ *	static int __init __driver##_init(void) \
+ *	{ \
+ *		return __register(&(__driver) , ##__VA_ARGS__); \
+ *	} \
+ *	module_init(__driver##_init); \
+ *	static void __exit __driver##_exit(void) \
+ *	{ \
+ *		__unregister(&(__driver) , ##__VA_ARGS__); \
+ *	} \
+ *	module_exit(__driver##_exit);
 */
 module_i2c_driver(i2c_slave_mqueue_driver);
 
