9822407878a1bb4b61e295449dedf6bc0800564d
albertZhuo420 <mr.zhuo420@outlook.com>
2023-10-02 12:05:14 +0800

    优化: 添加路径的判断

--- my_install_modules.sh
+++ my_install_modules.sh
@@ -3,27 +3,37 @@
 function print_usage()
 {
 	echo "***************Usage Start ***************"
-	echo "	./$0 b: Use busybox"
+	echo "	$0 b: Use busybox"
 	echo "		or"
-	echo "	./$0 s: Use systemd"
+	echo "	$0 s: Use systemd"
 	echo "***************Usage End *****************"
 }
 
 BUSYBOX_FS_PATH="/home/zhuo/work/server-dir/nfs/rootfs/buildroot-2023.05.1/busybox/usr"
-SYSTEMD_FS_PATH="/home/zhuo/work/server-dir/nfs/rootfs/buildroot-2023.05.1/systemd/usr"
+SYSTEMD_FS_PATH="/home/zhuo/work/server-dir/nfs/rootfs/buildroot/systemd/arm/imx/usr"
 
 if [ $# == 0 ]; then
 	print_usage;
 	exit 1
 else
 	if [ $1 == 'b' ]; then 
-		make modules_install INSTALL_MOD_PATH=$BUSYBOX_FS_PATH
+		if [ -d $BUSYBOX_FS_PATH ]; then
+			make modules_install INSTALL_MOD_PATH=$BUSYBOX_FS_PATH
+		else 
+			echo "BUSYBOX Dir path does not exits."
+			exit 2
+		fi
 		# echo $BUSYBOX_FS_PATH
 	elif [ $1 == 's' ]; then
-		make modules_install INSTALL_MOD_PATH=$SYSTEMD_FS_PATH
+		if [ -d $SYSTEMD_FS_PATH ]; then
+			make modules_install INSTALL_MOD_PATH=$SYSTEMD_FS_PATH
+		else
+			echo "SYSTEMD Dir path does not exits."
+			exit 3
+		fi
 		# echo $SYSTEMD_FS_PATH
 	else
 		print_usage
-		exit 2
+		exit 4
 	fi
 fi
